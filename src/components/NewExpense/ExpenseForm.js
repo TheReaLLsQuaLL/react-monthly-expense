7200ed525303df76b0d7650845d7f3e429160bff
TheReaLLsQuaLL <150316039@ogr.cbu.edu.tr>
2022-08-28 19:02:39 +0300

    feat- Added the expense adding UI also added change Title button

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,27 @@
+import "./ExpenseForm.css";
+
+function ExpenseForm() {
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text"></input>
+        </div>
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="number" min="0.01" step="0.01"></input>
+        </div>
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="date" min="1997-01-01" max="2100-12-31"></input>
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+}
+
+export default ExpenseForm;
